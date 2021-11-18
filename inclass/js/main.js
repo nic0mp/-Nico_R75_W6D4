43281c30dded729b7f426a529638582f37b59569
nic0mp <nico.mp215@gmail.com>
2021-11-18 13:53:52 -0600

    updates

--- inclass/js/main.js
+++ inclass/js/main.js
@@ -22,3 +22,38 @@ const getToken = async () =>{
     console.log(data)
     return data.access_token
 }
+
+// Function to get the song info when the image is clicked
+/**
+ * @param img_index
+ * @param item_index
+ * 
+ * Function gets song from spotify using the image index of our gallery.
+ * Then finds the correct item_index inside of the JSON response data from Spotify
+ * which will produce a preview url that will be used to play song from soundtrack.
+ * 
+ */
+
+async function clickedEvent(img_index,item_index){
+    let track = document.getElementsByTagName('img')[img_index].attributes[1].value;
+    // Get token
+    let token = await getToken();
+
+    let headers = new Headers([
+        ['Content-Type', 'application/json'],
+        ['Accept', 'application/json'],
+        ['Authorization', `Bearer ${token}`]
+    ])
+
+    let request = new Request(`https://api.spotify.com/v1/search?q=${track}&type=track&limit=15`, {
+        method: 'GET',
+        headers: headers
+    })
+
+    let result = await fetch(request);
+
+    let response = await result.json();
+    console.log(response)
+    song = response.tracks.items[item_index].preview_url
+}
+
